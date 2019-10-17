6a7c86d317e5fc0fbf53a4553298a53a5af60249
lenartd <43435805+lenartd@users.noreply.github.com>
2019-10-17 20:37:58 +0200

    added solving method, checking fixes

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,33 @@ public class Main
     public static void main(String[] args)
     {
 
+        int[][] board =
+                {
+                        {1,0,0,0,2,6,0,3,9},
+                        {0,0,8,7,0,0,6,0,1},
+                        {0,0,0,0,0,0,2,0,0},
+                        {4,0,0,9,0,0,5,1,6},
+                        {0,0,0,0,0,0,0,0,0},
+                        {7,9,6,0,0,4,0,0,8},
+                        {0,0,9,0,0,0,0,0,0},
+                        {3,0,1,0,0,2,9,0,0},
+                        {6,4,0,1,8,0,0,0,3},
+                };
+
+        Sudoku sudo = new Sudoku();
+        sudo.setBoard(board);
+        sudo.printOut();
+
+        System.out.println(" ");
+
+        if (sudo.SolveSudoku())
+        {
+            sudo.printOut();
+        }
+        else
+            {
+                System.out.println("failed");
+            }
     }
+
 }
---
+++ src/Sudoku.java
@@ -0,0 +1,95 @@
+public class Sudoku
+{
+    private int myBoard[][];
+
+    public void setBoard(int[][] board)
+    {
+        myBoard = new int[9][9];
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                myBoard[i][j] = board[i][j];
+            }
+        }
+    }
+
+    private boolean checkRow(int row, int num)
+    {
+        for(int i = 0; i<9; i++)
+        {
+            if(myBoard[row][i] == num){return true;}
+        }
+        return false;
+    }
+
+    private boolean checkColumn(int column, int num)
+    {
+        for(int i = 0; i<9; i++)
+        {
+            if(myBoard[i][column] == num){return true;}
+        }
+        return false;
+    }
+
+    private boolean checkBox(int row, int column, int number)
+    {
+        int r = row - row % 3;
+        int c = column - column % 3;
+
+        for (int i = r; i < r + 3; i++)
+        {
+            for (int j = c; j < c + 3; j++)
+            {
+                if (myBoard[i][j] == number)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkConditions(int row, int column, int num)
+    {
+        return !checkRow(row, num)  &&  !checkColumn(column, num)  &&  !checkBox(row, column, num);
+    }
+
+    public boolean SolveSudoku()
+    {
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+            {
+                if(myBoard[i][j] == 0)
+                {
+                    for(int k = 1; k<=9; k++)
+                    {
+                        if(checkConditions(i, j, k))
+                        {
+                            myBoard[i][j] = k;
+                            if(SolveSudoku()){return true;}
+                            else{myBoard[i][j] = 0;}
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printOut()
+    {
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+            {
+                System.out.print(myBoard[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
